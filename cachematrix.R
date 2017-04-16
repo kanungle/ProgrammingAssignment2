62fd2ce9200b5599dee1f664b13e5d8f37f69d7e
kanungle <neil.kanungo@gmail.com>
2017-04-16 00:04:27 -0500

    Added comments to R script

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This function takes in a matrix for the cache and creates a list for get, set, getInv, and getInv.
+## This cache can then be retrieved in subsequent function calls so R does not have to re-invert 
+## the matrix, thus saving resources.
 
 makeCacheMatrix <- function(x = matrix()) {
   I <- NULL
@@ -12,13 +12,16 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function() x
   setInv <- function(solve) I <<- solve
   getInv <- function() I
-  list(set = set, get = get,
+  list(set = set, 
+       get = get,
        setInv = setInv,
        getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## This is the solving function that determines if the inverse of a matrix already exists. If it
+## does, then R simply retrieves that inverted matrix from the Cache. If it does not, R then
+## calculates the inverted matrix, stores it in the cache, and also returns it to the user.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
